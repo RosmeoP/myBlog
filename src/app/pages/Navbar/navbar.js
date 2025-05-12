cc3328d8a7a6299df624eb89c636b137ed6a7b87
Mauricio <vrosmeo@gmail.com>
2025-05-11 21:46:52 -0600

    making the first page of the blog

---
+++ src/app/pages/Navbar/navbar.js
@@ -0,0 +1,9 @@
+
+
+export default function Navbar() {
+    return (
+      <header className="fixed top-0 w-full p-4 bg-white/80 backdrop-blur">
+        <a href="/" className="text-lg font-bold">My Blog</a>
+      </header>
+    );
+  }
---
+++ src/app/pages/blogView/view.js
@@ -0,0 +1,12 @@
+
+export default function BlogView() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-black text-decoration-line: underline decoration-white">
+            <h2 className="text-[100px] text-white font-bold tracking-wide" >
+                WELCOME TO ROSMEO'S <br /> PERSONAL BLOG
+            </h2>
+           
+          
+        </div>
+    )
+}
