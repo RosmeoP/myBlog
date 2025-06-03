a98d17d04bbcfa7f9bc7c4f87e1d703b3cfaaac6
Mauricio <vrosmeo@gmail.com>
2025-06-02 21:05:10 -0600

    feat: update author name in existing post and add new post about UX design

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -1,5 +1,6 @@
 // lib/posts.js
 
+
 export const posts = [
   {
     id: '1',
@@ -18,7 +19,7 @@ export const posts = [
     content: 'Discipline is the bridge between goals and accomplishment. It’s the ability to push through when motivation wanes. While motivation can get you started, discipline keeps you going. Because when the initial excitement fades, it’s discipline that will carry you through the tough times. It’s about creating habits and routines that support your goals, even when you don’t feel like it. Remember, motivation is fleeting, but discipline is a skill that can be developed and strengthened over time, like Goggins would say, stay hard!',
     imageUrl: "https://i.pinimg.com/736x/c9/84/94/c98494e32acd2f827ee4d1e9e8500bfd.jpg",
     category: 'Tech',
-    author: { name: 'You' },
+    author: { name: 'Rosmeo' },
     date: '2025-05-10',
     excerpt: 'F*CK motivation, discipline is the key to success.',
     readTime: '4 min',
@@ -67,7 +68,18 @@ export const posts = [
     excerpt: 'A powerful quote from David Goggins about pushing your limits.',
     readTime: '1 min',
   },
-  
+  {
+    id: '7',
+    title: 'Learning about UX Design',
+    content: 'UX design is all about creating user-friendly interfaces that enhance the user experience. It involves understanding user needs, behaviors, and motivations to create products that are not only functional but also enjoyable to use. Im currently learning about the cicly of UX design, which includes research, design, prototyping, testing, and implementation. This process ensures that the final product meets user expectations and provides a seamless experience. You may be wondering why I am learning about UX design, well, I am currently working on a project that requires me to create a user-friendly interface for a web application. I want to ensure that the end product is intuitive and easy to navigate, so I am diving deep into the principles of UX design. Also a software engineer should know about UX design, it is a valuable skill that can set you apart in the tech industry. ',
+    imageUrl: 'https://media.licdn.com/dms/image/v2/D5612AQGAiytCB9sK8A/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1707194321088?e=2147483647&v=beta&t=0aiOooMn8BHc8o_yWPZi5MEJ-lPiqNxa_PHWEdy9158',
+    category: 'Development',
+    author: { name: 'Rosmeo' },
+    date: '2025-06-01',
+    excerpt: 'Exploring the principles of user experience design and its importance in creating intuitive interfaces.',
+    readTime: '6 min',
+  }
+
 ];
 
 
